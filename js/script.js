b4df2b43ac157db9bd4ba3bb01e9fe84ed753bed
CraciDenisStefan <crdenis13@gmail.com>
2023-05-18 17:03:37 +0200

    Creato una parte di html

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@ button.addEventListener(`click`, function(){
         console.log(input_km) 
     let sel_eta = document.getElementById(`eta`).value;
         console.log(sel_eta)  
-        
+
     let prezzo_km = (0.21);
     let prezzo = prezzo_km * input_km;
             console.log(prezzo)
             
     if(sel_eta === `Minorenne`) {
-        sconto_20 =  prezzo * 20 / 100 ;
+        let sconto_20 =  prezzo * 20 / 100 ;
         prezzo = (prezzo - sconto_20);
         console.log(prezzo)
         }else if(sel_eta === `Over 65`) {
-            sconto_40 =  prezzo * 40 / 100 ;
-            prezzo = (prezzo - sconto_40);
+        let sconto_40 =  prezzo * 40 / 100 ;
+        prezzo = (prezzo - sconto_40);
             console.log(prezzo)
         }
 })
